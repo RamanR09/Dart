3547b4f32d2fbe65173012ba0cd3b957ccf93f49
RamanR09 <134045938+RamanR09@users.noreply.github.com>
2023-09-20 19:20:04 +0530

    first comit

---
+++ Dart01/src/App.jsx
@@ -0,0 +1,33 @@
+import {
+  Loader,
+  Services,
+  Footer,
+  Transection,
+  Welcome,
+  Navbar,
+} from "./Componenets";
+
+import { useState } from "react";
+
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        {/* <Header /> */}
+        <Navbar />
+        <Welcome />
+      </div>
+      <div>
+        <Services />
+        <Transection />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Dart01/src/Componenets/Navbar.jsx
@@ -0,0 +1,14 @@
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import logo from "./images/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex md:justify-center justify-middle item-center p-4">
+      {" "}
+      H--H
+    </nav>
+  );
+};
+export default Navbar;
