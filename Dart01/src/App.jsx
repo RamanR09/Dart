b065569c8e5176cf155c9e88626d32be1d130f15
RamanR09 <134045938+RamanR09@users.noreply.github.com>
2023-11-16 22:18:28 +0530

    first comit Front end

--- Dart01/src/App.jsx
+++ Dart01/src/App.jsx
@@ -7,25 +7,36 @@ import {
   Navbar,
 } from "./Componenets";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
+  useEffect(() => setCounter(() => count * 2), [count]);
+  const handleClick = () => {
+    setCount(() => count + 1);
+  };
 
   return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
+    <div className="min-h-screen bg-black">
+      <div className="  bg-gradient-to-r from-orange-600 via-red-500 to-red-600 ...   hover:bg-gradient-to-l">
         {/* <Header /> */}
         <Navbar />
-        <Welcome />
+        {/* <Welcome /> */}
       </div>
-      <div>
+      <div className="bg-[#0b0212]">
+        <Welcome />
+        {/* <Welcome />
+        <Welcome /> */}
         <Services />
         <Transection />
         <Footer />
       </div>
+      <h1> {count}</h1>
+      <button onClick={handleClick}> + </button>
+      <h1> {counter}</h1>
     </div>
   );
 };
